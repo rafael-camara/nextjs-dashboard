fd0ca5a4f86e5d84842effa3bbea08aa72551834
Rafael Câmara <rafaelcamara.377@gmail.com>
2024-06-30 13:37:48 -0300

    feat: get data with server component

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -13,19 +13,26 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default async function CardWrapper() {
+type TCardWrapper = {
+  numberOfInvoices: number
+  numberOfCustomers: number
+  totalPaidInvoices: number
+  totalPendingInvoices: number
+}
+
+export default async function CardWrapper({ totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers }: TCardWrapper) {
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
